4a466097574b026d413817d83b740d6f3befe84a
wancynthianjee <wanjirucynthiaanjerii@gmail.com>
2023-08-24 08:52:08 +0300

    commit

---
+++ app/src/main/java/com/example/product/database/productDao.kt
@@ -0,0 +1,4 @@
+package com.example.product.database
+
+class productDao {
+}
---
+++ app/src/main/java/com/example/product/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.product.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.product.R
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    fun products(){
+
+
+    }
+}
